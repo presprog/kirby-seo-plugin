6ff8e72c587c9b3f0380cc6d166216a7450ab18d
Benedict Massolle <bm@presentprogressive.de>
2021-12-01 08:14:17 +0100

    Handle multilingual sitemap generation

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 use Kirby\Cms\App as Kirby;
+use Kirby\Cms\Language;
 use Kirby\Http\Response;
+use Kirby\Http\Url;
 use Kirby\Toolkit\Xml;
 use PresProg\KirbyMeta\PageMeta;
 
@@ -31,17 +33,25 @@ Kirby::plugin('presprog/meta', [
             'action' => function () {
                 $robots = 'User-agent: *' . PHP_EOL;
                 $robots .= 'Allow: /' . PHP_EOL;
-                $robots .= 'Sitemap: ' . url('sitemap.xml');
+
+                if (kirby()->multilang()) {
+                    /** @var Language $language */
+                    foreach (kirby()->languages() as $language) {
+                        $robots .=  Url::makeAbsolute('/sitemap.xml', $language->url()) . PHP_EOL;
+                    }
+                } else {
+                    $robots .= 'Sitemap: ' . url('sitemap.xml');
+                }
 
                 return kirby()
                     ->response()
                     ->type('text')
                     ->body($robots);
             }
-        ],
-        [
+        ], [
             'pattern' => 'sitemap.xml',
-            'action' => function () {
+            'language' => '*',
+            'action' => function ($language) {
                 $templatesIncludeList = option('presprog.meta.sitemap.templatesInclude', []);
                 $pagesIncludeList = option('presprog.meta.sitemap.pagesInclude', []);
                 $pagesExcludeList = option('presprog.meta.sitemap.pagesExclude', []);
@@ -49,7 +59,7 @@ Kirby::plugin('presprog/meta', [
                 $excludeListPattern = '!^(?:' . implode('|', $pagesExcludeList) . ')$!i';
 
                 $cache = kirby()->cache('pages');
-                $cacheId = 'sitemap.xml';
+                $cacheId = (kirby()->multilang()) ? 'sitemap.' . kirby()->language()->code() . '.xml' : 'sitemap.xml';
 
                 if (!$sitemap = $cache->get($cacheId, [])) {
 
